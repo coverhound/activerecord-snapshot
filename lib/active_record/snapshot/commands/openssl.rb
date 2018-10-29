bd080dd720c055a5419938d20d6fab6272adcd0b
cobyrne09 <co_byrne@yahoo.com>
2018-10-29 14:33:31 -0700

    forced snapshots to be taken and downloaded with sha256

--- lib/active_record/snapshot/commands/openssl.rb
+++ lib/active_record/snapshot/commands/openssl.rb
@@ -3,7 +3,7 @@ module ActiveRecord
     class OpenSSL
       def self.encrypt(input:, output:)
         system(<<-SH)
-        nice openssl aes-256-cbc \\
+        nice openssl aes-256-cbc -md sha256 \\
           -in #{input} \\
           -out #{output} \\
           -kfile #{ActiveRecord::Snapshot.config.ssl_key}
@@ -12,7 +12,7 @@ module ActiveRecord
 
       def self.decrypt(input:, output:)
         system(<<-SH)
-        nice openssl enc -d -aes-256-cbc \\
+        nice openssl enc -d -aes-256-cbc -md sha256 \\
           -in #{input} \\
           -out #{output} \\
           -kfile #{ActiveRecord::Snapshot.config.ssl_key}
